9c2c8c7c8665003311512f3be2b8143a97e42e15
vraspar <v2parikh@uwaterloo.ca>
2025-11-22 19:43:29 -0300

    add mock test backend

--- backend/src/routes/accountRoutes.ts
+++ backend/src/routes/accountRoutes.ts
@@ -4,8 +4,8 @@ import * as accountController from '../controllers/accountController';
 const router = express.Router();
 
 // Account routes
-router.get('/info', accountController.getAccountInfo);
-router.post('/deposit', accountController.deposit);
-router.post('/refund', accountController.requestRefund);
+// router.get('/info', accountController.getAccountInfo);
+// router.post('/deposit', accountController.deposit);
+// router.post('/refund', accountController.requestRefund);
 
 export default router; 
